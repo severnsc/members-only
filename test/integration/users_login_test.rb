6f99e6fca45d1df2a8e06e49e34ee9e5f6662497
Chris Severns <severnsc@gmail.com>
2017-02-03 11:50:03 -0500

    Add login test

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = users(:chris)
+	end
+
+	test "Try to login with invalid login information then valid" do
+		get login_path
+		assert_template 'sessions/new'
+		#Correct Email, wrong password
+		post login_path, params: { session: { email: @user.email,
+											  password: "foobar"}}
+		assert_template 'new'
+		assert_not flash.empty?
+		#Wrong email, correct password
+		post login_path, params: { session: { email: "me@example.com",
+											  password: "password"}}
+		assert_template 'new'
+		assert_not flash.empty?
+		#Correct email and password
+		post login_path, params: { session: { email: @user.email,
+											  password: "password"}}
+		assert is_logged_in?
+		assert_redirected_to @user
+		follow_redirect!
+		assert_template 'users/show'
+	end
+end
