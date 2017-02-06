ffaab58baacefe1ffce554ee65a41527d3d73963
Chris Severns <severnsc@gmail.com>
2017-02-06 15:48:52 -0500

    Add post model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	@user = users(:chris)
+  	@post = Post.new(body: "This is a post", user_id: @user.id)
+  end
+
+  test "post should be valid" do
+  	assert @post.valid?
+  end
+
+  test "body should be present" do
+  	@post.body = "   "
+  	assert_not @post.valid?
+  end
+
+  test "body should be 255 chars max" do
+  	@post.body = "a"*256
+  	assert_not @post.valid?
+  end
+
+  test "user_id should be present" do
+  	@post.user_id = " "
+  	assert_not @post.valid?
+  end
 end
